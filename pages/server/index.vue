1655b9eb207741ad201e2b93c37d2dcbea65c8dd
infernalslam <infernal-slam@hotmail.com>
2017-09-01 16:49:27 +0700

    css

--- pages/server/index.vue
+++ pages/server/index.vue
@@ -1,21 +1,9 @@
 <template>
-<div>
-  <section class="container">
-    <h1 class="title">
-      Server
-    </h1>
-    <div><youtube :video-id="id" :player-vars="{autoplay: 1}"></youtube></div>
-    <ul class="users">
-    <br> 
-    LIST :
-    <div v-for="(src, i) in data" :key="src.id">
-       ชื่อเรื่อง  {{ src.title }} <br>
-       รหัสวิดิโอ  {{ src.id }} <br>
-       <img :src="src.img">
-       <hr>
+  <div>
+    <div class="layout">
+      <div class="left">1</div>
+      <div class="right">2</div>
     </div>
-    </ul>
-  </section>
   </div>
 </template>
 
@@ -31,7 +19,7 @@ export default {
     return {
       socket: null,
       data: [],
-      id: 'kSEHbNMwumA'
+      id: '4OrCA1OInoo'
     }
   },
   async mounted () {
@@ -44,18 +32,58 @@ export default {
 </script>
 
 <style scoped>
-.title
-{
-  margin: 30px 0;
+.layout {
+  background-color: black;
 }
-.users
-{
-  list-style: none;
-  margin: 0;
-  padding: 0;
+.left {
+  background-color: red;
+  display: inline-block;
+  width: 20%;
+  height: 100vh;
 }
-.user
-{
-  margin: 10px 0;
+.right {
+  background-color: green;
+  display: inline-block;
+  height: 100vh;
+  width: 80%;
 }
 </style>
+
+<!-- <div class="container">
+  <youtube :video-id="id" :player-vars="{autoplay: 1}"   player-width="100%" player-heigh="100%" ></youtube>
+</div> -->
+
+<!--<section class="container">
+  <h1 class="title">
+    Server
+  </h1>
+  <div><youtube :video-id="id" :player-vars="{autoplay: 1}"></youtube></div>
+  <ul class="users">
+  <br>
+  LIST :
+  <div v-for="(src, i) in data" :key="src.id">
+     ชื่อเรื่อง  {{ src.title }} <br>
+     รหัสวิดิโอ  {{ src.id }} <br>
+     <img :src="src.img">
+     <hr>
+  </div>
+  </ul>
+</section> -->
+<!-- <div>
+  <nav class="container">
+      <ul>
+        <li><a href="#">London</a></li>
+        <li><a href="#">Paris</a></li>
+        <li><a href="#">Tokyo</a></li>
+      </ul>
+    </nav>
+  <div class="youtubesize">
+      <youtube  class="youtubesize" :video-id="id" :player-vars="{autoplay: 1}"></youtube>
+  </div>
+</div> -->
+
+<!-- <div style="position: fixed; z-index: -99; width: 100%; height: 100%">
+     <iframe frameborder="0" height="100%" width="100%"
+       src="https://youtube.com/embed/ID?autoplay=1&controls=0&showinfo=0&autohide=1">
+     </iframe>
+</div> -->
